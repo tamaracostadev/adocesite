ac63d003b384dde747eec5afc5d92f8cff6077ec
Tamara Cristina <tdesouzacosta@gmail.com>
2020-12-30 19:48:11 -0300

    finalização

---
+++ js/script.js
@@ -0,0 +1,5 @@
+new SimpleSlide({slide: 'feedback1',nav: false,time: 5000, pauseOnHover: true, });
+new SimpleSlide({slide: 'feedback2',nav: false,time: 5000, pauseOnHover: true, });
+new SimpleSlide({slide: 'feedback3',nav: false,time: 5000, pauseOnHover: true, });
+new SimpleAnime();
+
